e41b5a2f291722d011b91d7922ccfb1c7eeacd60
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2022-11-06 00:11:10 +0200

    create files for task 1.1 & 1.2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const imagesEl = galleryItems
+  .map(
+    item =>
+      `<div class = "gallery__item"><a class="gallery__link" href="${item.original}">
+  <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+  />
+</a>
+</div>`
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', imagesEl);
+
+galleryEl.addEventListener('click', imageClick);
+
+function imageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
